1c3a25cf65e19dfb044383a7a8a4f9a8c3f22a6d
zhaopengfei <zpf@zhaopengfeideMacBook-Pro.local>
2020-03-15 22:29:45 +0800

    fix Fibonacci

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,17 +1,19 @@
 #include "stdio.h"
 
 // 斐波那契数列迭代实现
-int main_iter(){
-    int a[40];
+int main_iter(int size){
+    int a[size];
     int i;
     a[0] = 0;
     a[1] = 1;
-    for(i=2; i<40; i++){
+    for(i=2; i<size; i++){
         a[i] = a[i-1] + a[i-2];
     }
-    for(i=0; i<40; i++){
+    printf("斐波那契数列迭代实现结果:");
+    for(i=0; i<size; i++){
         printf("%d ", a[i]);
     }
+    printf("\n");
     return 1;
 }
 
@@ -24,7 +26,16 @@ int fib(int n){
 }
 
 int main(){
-    int n=10;
-    printf("%d", fib(n));
+    int i, n;
+    printf("请出入需要计算的斐波那契数列个数:");
+    scanf("%d", &n);
+    printf("斐波那契数列递归实现结果:");
+    for(i=0; i<=n; i++){
+        printf("%d ", fib(i));
+    }
+    printf("\n");
+
+    main_iter(n);
+
     return 1;
 }
